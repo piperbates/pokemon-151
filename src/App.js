e372271b8c61b842caf5495db98d2e40a0425f30
Piper <bates.sarah42@gmail.com>
2022-10-20 00:02:55 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,129 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import React, {useState, useEffect} from 'react';
+import TableData from './components/TableData';
+import { pokemonList } from './pokemon-list';
+
+import "./App.css"
+
+export default function App() {
+
+  const [userInput, setUserInput] = useState("");
+  const [gameList, setGameList] = useState([]);
+  const [gameOngoing, setGameOngoing] = useState(false);
+
+  useEffect(() => {
+    pokemonList.map((pokemon)=>{
+      if(userInput.toLowerCase() === pokemon.name.toLowerCase()){
+        if(gameList.length === 0){
+          setGameList([...gameList, pokemon.name.toLowerCase()])
+          setUserInput("")
+          return null
+        } else {
+          if(gameList.includes(userInput.toLowerCase())){
+            console.log(`${userInput} already exists in array`)
+            return null
+          } else {
+            setGameList([...gameList, pokemon.name.toLowerCase()])
+            setUserInput("")
+            return null
+          }
+      } 
+    }
+    return null
+  })}, [userInput, gameList])
+
+  //counter
+  useEffect(()=>{
+    if(gameList === 151 && !gameOngoing){
+      setGameOngoing(false)
+    } else {
+      // setGameOngoing(true)
+    }
+  }, [gameList, gameOngoing]
+  )
+
+  const pokemonGroup = [
+    pokemonList.slice(0, 16),
+    pokemonList.slice(16, 32),
+    pokemonList.slice(32, 48),
+    pokemonList.slice(48, 64),
+    pokemonList.slice(64, 80),
+    pokemonList.slice(80, 96),
+    pokemonList.slice(96, 112),
+    pokemonList.slice(112, 128),
+    pokemonList.slice(128, 144),
+    pokemonList.slice(144)
+  ]
+
+
+
+  const renderGameStatus = () => 
+    gameOngoing 
+      ? `${gameList.length} / 151`
+      : gameList.length ? `You got ${gameList.length} out of 151. Start typing below to try again!` : "Start typing below to start new game"
+
+    const renderButton = () =>
+      !gameOngoing && gameList.length 
+        ? <button onClick={()=>resetGame()}>Reset Game</button>
+        : <button onClick={()=>endGame()} disabled={!gameOngoing}>End Game</button>
+
+
+  const startNewGame = () => {
+    setGameOngoing(true);
+    setGameList([]);
+  }
+
+  const endGame = () => {
+    //thanos says hi
+    setGameOngoing(false);
+    setUserInput("");
+  }
+
+  const resetGame = () => {
+    setGameList([]);
+    setGameOngoing(false);
+    setUserInput("");
+  }
+
+  const handleOnChange = (value) => {
+    setUserInput(value)
+    if(!gameOngoing){
+      startNewGame()
+    }
+  }
+
+  // const autoFill = () => {
+  //   setGameOngoing(true)
+  //   console.log(pokemonList)
+  //   pokemonList.map((pokemon)=>{
+  //     setGameList([...gameList, pokemon.name.toLowerCase()])
+  //     console.log(gameList)
+  //     return null
+  //   })
+  // }  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div className="game-status">{renderGameStatus()}</div>
+  
+      <input 
+        value={userInput}
+        onChange={(e)=>handleOnChange(e.target.value)}
+      />
+
+      <table>
+      {pokemonGroup.map((list)=>{ 
+        return <tr>
+          {list.map((pokemon)=>{
+            return <TableData pokemon={pokemon} gameList={gameList} gameOngoing={gameOngoing}/>
+          })}
+        </tr>
+      })}
+      </table>
+
+        {renderButton()}
+
+        {/* <button onClick={()=>autoFill()}>Test</button> */}
     </div>
   );
-}
-
-export default App;
+};
---
+++ src/components/TableData.js
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from 'react'
+
+export default function TableData({ pokemon, gameList, gameOngoing }) {
+    const [displayName, setDisplayName] = useState("");
+    const [answered, setAnswered] = useState(false);
+
+    useEffect(()=> {
+        if(gameOngoing){
+            if(gameList!==0){
+                if(gameList.includes(pokemon.name.toLowerCase())){
+                    setDisplayName(pokemon.name);
+                    setAnswered(true);
+                    return
+                }
+                setDisplayName("")
+            }
+        }
+        if(!gameOngoing && gameList.length === 0){
+            setAnswered(false)
+        }
+    }, [gameList, pokemon, gameOngoing])
+
+    useEffect(()=>{
+        if(!gameOngoing && !answered && gameList.length){
+            setDisplayName(pokemon.name)
+        } else if(gameList.length === 0){setDisplayName("")}
+    }, [gameOngoing, answered, gameList, pokemon])
+
+    const shouldUnansweredBeRed = !gameOngoing && !answered && gameList.length ? true : false
+
+    const styleAnswered =  {color: "black"}
+    const styleIncorrect = shouldUnansweredBeRed ? {color: "red"} : styleAnswered
+
+    return (
+        <td className="td">
+            <span 
+                style={!gameOngoing 
+                            ? answered ? styleAnswered : styleIncorrect
+                            : {color:"black"}}
+            >{pokemon.dex}: {displayName}</span>
+        </td>
+    )
+}
